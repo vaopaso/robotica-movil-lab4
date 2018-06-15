27839815efe3e7dfc1b7d9b98088e26785404abe
Vicente Opaso Valenzuela <vicenteopaso@MacBook-Pro-de-Vicente.local>
2018-06-15 14:10:31 -0400

    path finding plot

--- src/main.py
+++ src/main.py
@@ -9,12 +9,18 @@ from nav_msgs.msg import Odometry
 import copy
 import numpy as np
 import math
+import cv2
 import json
 from path_finding.data_structure import Matrix
 from localization.c_space import isPainted,paintNeighbours,read_pgm,print_matrix
 
 # from blur_map import callback_scan, first_generation_particles
 # from c_space import c_spce, read_pgm
+
+_RED = [0, 0, 255]
+_BLUE = [255, 0, 0]
+_GREEN = [0, 255, 0]
+_BLACK = [0, 0, 0]
 
 class PathFinding:
 
@@ -24,6 +30,8 @@ class PathFinding:
         self.height_pixels = self.mapa.shape[0]
         self.bottom_left_origin = bottom_left_origin
         self.resolution = resolution
+
+        self.img = cv2.imread(path_map)
         #cspace
         radius = 0.2/self.resolution
         self.cspace_matrix = copy.deepcopy(self.mapa)
@@ -70,21 +78,51 @@ class PathFinding:
         else:
             return self.matrix.findPath(start_node, goal_node)
 
+    def plotPathFound(self, nodes):
+        # rad = self.particle_radius*self.display_resolution
+        image = copy.deepcopy(self.img)
         
+        if nodes is not None:
+            for node in nodes:
+                cv2.rectangle(image,(node.j,node.i),(node.j,node.i),_BLUE, 1)
+        # if nodes is not None:
+        #     for node in nodes:
+        #         i, j = mapCoords_to_pixel(particle['x'], particle['y'], image.shape[0], image.shape[1] , self.origin, self.display_resolution)
+        #         # print(particle['x'],i,particle['y'],j)
+        #         if isValidPixel(i, j, image.shape[0], image.shape[1]):
+        #             # draw robot circle
+        #             cv2.circle(image, (j,i), self.particle_radius, _RED, -1)
+        #             # draw direction
+        #             y_prima = rad*np.sin(particle['theta']) + particle['y']
+        #             x_prima = rad*np.cos(particle['theta']) + particle['x']
+        #             # print(x_prima,y_prima)
+        #             # x_prima,y_prima = mapCoords_to_pixel(x_prima,y_prima,image.shape[0],image.shape[1],bottom_left_origin,resolution)
+        #             # y_prima = int(round(particle_radius*np.sin(particle['theta'])))
+        #             # x_prima = int(round(particle_radius*np.cos(particle['theta'])))
+        #             x_prima,y_prima = mapCoords_to_pixel(x_prima,y_prima,image.shape[0],image.shape[1],self.origin,self.display_resolution)
+        #             cv2.line(image, (j,i), (y_prima,x_prima), _BLACK, 1)
 
+        # Display window
+        cv2.namedWindow('Path Finding', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Path Finding', 600,600)
+        cv2.startWindowThread()
+        cv2.imshow('Path Finding', image)
+
+        
+rospy.init_node("path_finding")
 
 move = Move()
 obst = ObstacleDetector()
 
 bottom_left_origin = [0,0]
 resolution = 0.1
-path_finding = PathFinding('../include/map.pgm', bottom_left_origin, resolution)
+path_finding = PathFinding('../../include/map.pgm', bottom_left_origin, resolution)
 
 rospy.sleep(1)
 
 
-rospy.Subscriber("/obstaculos", String, move.callback_obstaculo)
-rospy.Timer(rospy.Duration(0.03),move.controlled_tick)
+# rospy.Subscriber("/obstaculos", String, move.callback_obstaculo)
+# rospy.Timer(rospy.Duration(0.03),move.controlled_tick)
 
 initial_pose = {'x':0.5,'y':1.3,'theta':0}
 goal_pose = {'x':2.1,'y':0.5,'theta':0}
@@ -94,6 +132,8 @@ if nodes:
         print(i)
 else:
     print('Error. nodes no valido:', nodes)
+
+path_finding.plotPathFound(nodes)
     
 # rospy.sleep(1)
 
